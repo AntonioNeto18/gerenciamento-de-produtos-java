f1155d8e17ef924440d31358a2d4f77dad9a2657
Antonio <antoniojacintoa@gmail.com>
2026-02-07 16:35:06 -0300

    chore: liberando acesso da api atrabes do cors

--- backend/src/main/java/com/antonio/gerenciamento_de_produtos/dto/RequestUpdateProductDTO.java
+++ backend/src/main/java/com/antonio/gerenciamento_de_produtos/dto/RequestUpdateProductDTO.java
@@ -1,21 +1,23 @@
 package com.antonio.gerenciamento_de_produtos.dto;
 
 import com.antonio.gerenciamento_de_produtos.entity.Product;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 import java.time.LocalDateTime;
 
 public record RequestUpdateProductDTO (
-        String name,
-        String description,
-        @Positive(message = "O preço precisa ser positivo.") Double price,
-        @Positive(message = "A quantidade no estoque precisa ser positiva.") Integer stock
+        @NotBlank(message = "O nome é obrigatório.") String name,
+        @NotBlank(message = "A descrição é obrigatória.") String description,
+        @NotNull(message = "O preço é obrigatório.") @Positive(message = "O preço precisa ser positivo.") Double price,
+        @NotNull(message = "A quantidade no estoque é obrigatória.") @Positive(message = "A quantidade no estoque precisa ser positiva.") Integer stock
 ) {
     public Product toEntity (Product product) {
-        product.setName(this.name() != null ? this.name() : product.getName());
-        product.setDescription(this.description() != null ? this.description() : product.getDescription());
-        product.setPrice(this.price() != null ? this.price() : product.getPrice());
-        product.setStock(this.stock() != null ? this.stock() : product.getStock());
+        product.setName(this.name());
+        product.setDescription(this.description());
+        product.setPrice(this.price());
+        product.setStock(this.stock());
         product.setUpdatedAt(LocalDateTime.now());
         return product;
     }
